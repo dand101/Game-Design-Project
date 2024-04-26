f1e024af79e85e6c3507cf11949bb06fde82d00c
dand101 <dandodun@gmail.com>
2024-04-26 17:55:16 +0300

    gun stats persist through lvls

--- GD-proj/Assets/Scripts/LevelManagement/LevelManagement.cs
+++ GD-proj/Assets/Scripts/LevelManagement/LevelManagement.cs
@@ -1,21 +1,46 @@
+using System;
 using UnityEngine;
 
 
 public class LevelManagement : MonoBehaviour
 {
     public LevelScriptableObject LevelScriptableObject;
-    
+    public GunStatsManager gunStatsManager;
+
+
+    private void Start()
+    {
+        if (LevelScriptableObject == null)
+        {
+            Debug.LogError("LevelScriptableObject is not set in the LevelManagement script. wtf");
+        }
+
+        if (gunStatsManager == null)
+        {
+            Debug.LogError("GunStatsManager is not set in the LevelManagement script. what 2");
+        }
+
+        if (LevelScriptableObject.currentLevel == 1)
+        {
+            gunStatsManager.setAllGunsStart();
+        }
+        else
+        {
+            gunStatsManager.keepGunStats();
+        }
+    }
+
     public void CompleteLevel()
     {
         LevelScriptableObject.CompleteLevel();
         Debug.Log("Level completed. Current level: " + LevelScriptableObject.currentLevel);
     }
-    
+
     public void ResetProgress()
     {
         LevelScriptableObject.ResetProgress();
     }
-    
+
     public int GetCurrentLevel()
     {
         return LevelScriptableObject.currentLevel;
